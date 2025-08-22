1810bcc0d3afe4eddc5b6520be0f218e195fb076
unknown <phampho310nd@gmail.com>
2025-08-22 16:03:34 +0700

    Bai tap ngay 2

---
+++ PDPDay2/PDPDay2/Controllers/MyController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PDPDay2.Controllers
+{
+    public class MyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Sample()
+        {
+            return View();
+        }
+        public IActionResult Services()
+        {
+            return View();
+        }
+    }
+}
---
+++ PDPDay2/PDPDay2/Controllers/MyNewController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PDPDay2.Controllers
+{
+    public class MyNewController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
