6cf07717e30da37d47994d935f4941b0decf74c8
Phố Phạm <phampho310nd@gmail.com>
2025-09-04 11:40:56 +0700

    bai tap day 3

---
+++ PDPDay3View/PDPDay3View/ViewComponents/HotItem.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PDPDay3View.Models;
+
+namespace PDPDay3View.ViewComponents
+
+{
+    public class HotItem : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var hp = new List<Item>
+            {
+                new Item  { Name = "Nồi cơm", Img = "image/noicom.jpeg" },
+                new Item  { Name = "Nồi cơm", Img = "image/noicom.jpeg" },
+                new Item  { Name = "Nồi cơm", Img = "image/noicom.jpeg" }
+
+            };
+
+            return View(hp);
+        }
+    }
+}
